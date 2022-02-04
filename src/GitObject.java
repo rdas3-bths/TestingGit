2de13b6d1a047389edf01077cdc64fbb34c9162e
teacher <sample@sample.com>
2022-02-04 10:41:48 -0500

    Added new GitObject

---
+++ src/GitObject.java
@@ -0,0 +1,11 @@
+public class GitObject {
+    private int num;
+
+    public GitObject(int n) {
+        num = n;
+    }
+
+    public void doThing() {
+        System.out.println("I am git number:" + num);
+    }
+}
--- src/TestingGit.java
+++ src/TestingGit.java
@@ -1,5 +1,7 @@
 public class TestingGit {
     public static void main(String[] args) {
         System.out.println("Testing Git 123");
+        GitObject g = new GitObject(20);
+        g.doThing();
     }
 }
